09e7cf2ccfd05075f55e47e293887bf7bf1efe37
diegobonagurio <diego.bonagurio@gmail.com>
2020-10-16 16:27:42 -0300

    loading component

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -33,8 +33,6 @@ export default function OrphanageDetails() {
 
   const params = route.params as OrphanageDetailsRouteParams; 
   
-  console.log(params.id);
-  
   useEffect(() => {
     api.get(`orphanages/${params.id}`).then(response => {
       setOrpahanage(response.data);
